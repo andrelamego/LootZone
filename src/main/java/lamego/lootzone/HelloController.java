b7b849f27e5cd17b4977352fa3778350864b75d7
André Lamego <andreolamego@gmail.com>
2025-10-16 00:01:29 -0300

    1st commit

---
+++ src/main/java/lamego/lootzone/HelloController.java
@@ -0,0 +1,14 @@
+package lamego.lootzone;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
