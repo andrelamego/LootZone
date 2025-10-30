32dc2f19883a21a8d3964ea14ba5be4bf9fe372e
André Lamego <andreolamego@gmail.com>
2025-10-30 02:34:41 -0300

    Fixes -Agora é possivel mover a janela de login

--- src/main/java/lamego/lootzone/MainApp.java
+++ src/main/java/lamego/lootzone/MainApp.java
@@ -3,6 +3,10 @@ package lamego.lootzone;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -10,10 +14,22 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("app/login/login-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Pane root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        final double[] offset = new double[2];
+        root.setOnMousePressed(e -> {
+            offset[0] = e.getSceneX();
+            offset[1] = e.getSceneY();
+        });
+        root.setOnMouseDragged(e -> {
+            stage.setX(e.getScreenX() - offset[0]);
+            stage.setY(e.getScreenY() - offset[1]);
+        });
 
         stage.setTitle("LootZone");
-        stage.initStyle(StageStyle.UNDECORATED);
+        scene.setFill(Color.TRANSPARENT);// fundo totalmente transparente
+        stage.initStyle(StageStyle.UNDECORATED); // sem bordas do SO
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
